4b660a1a301ccdbdb23555b6862e923f9d4e951f
Andreas Waigand <mail@andreaswaigand.de>
2019-01-16 23:35:17 +0100

    Added vuetify for test

--- src/library-plugin.js
+++ src/library-plugin.js
@@ -1,8 +1,11 @@
 import HelloWorld from "./components/HelloWorld.vue";
 import TestingHyphen from "./components/TestingHyphen.vue";
+import Vuetify from 'vuetify';
+import 'vuetify/dist/vuetify.min.css';
 
 export default {
   install(Vue) {
+    Vue.use(Vuetify);
     Vue.component("hello-world", HelloWorld);
     Vue.component("t-hyphen", TestingHyphen);
   }
